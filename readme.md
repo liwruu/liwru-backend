979c8953ede44c7fd724b367ce1e34c8e4fa8fc8
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-05-23 15:08:45 -0500

    backend init

---
+++ readme.md
@@ -0,0 +1,5 @@
+BACKEND CONFIGURATION by wifituga :D
+installed dependecies command:{
+    npm i sequelize
+    npm i express morgan cors pg pg-hstore
+}
---
+++ src/database/database.js
@@ -0,0 +1,6 @@
+import Sequilize from 'sequelize';
+
+const sequelize = new Sequilize('liwru','postgres','mz3842m5a7  ', {
+    host: 'localhost',
+    dialect: 'postgres',
+});
