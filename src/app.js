0014d4f5d577acab81f427aa2e6244a22a3bdac2
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-05-24 01:19:20 -0500

    feat(backend): complete materialState controller and routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import authorRoutes from './routes/authors.routes.js';
 import editorialRoutes from './routes/editorials.routes.js';
 import formatRoutes from './routes/formats.routes.js';
-
+import materialStateRoutes from './routes/materials.state.routes.js';
 
 const app = express();
 
@@ -12,5 +12,6 @@ app.use(express.json()); //Servidor interpreta .json y guarda en un req.body
 app.use(authorRoutes);
 app.use(editorialRoutes);
 app.use(formatRoutes);
+app.use(materialStateRoutes);
 
 export default app;
