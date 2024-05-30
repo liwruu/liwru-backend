c3f5eed2ce12d0d53caabe757a7eb47e3a0065bd
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-05-30 10:50:21 -0500

    feat(backend): modify database connections

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,7 @@
 import Sequilize from 'sequelize';
+const { db } = require('./config.js');
 
-export const sequelize = new Sequilize('liwru','postgres','mz3842m5a7', {
-    host: 'localhost',
-    dialect: 'postgres',
+export const sequelize = new Sequilize(db.database, db.user, db.password, {
+    host: db.host,
+    dialect: db.dialect,
 });
