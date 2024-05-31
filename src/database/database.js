c92d25d5b0faca3a5733cf3ce0022dace7bad2f1
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-05-30 20:30:42 -0500

    fix(backend): fixed azure database connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,18 @@
 import Sequilize from 'sequelize';
-const { db } = require('./config.js');
+import dotenv from 'dotenv';
+dotenv.config()
 
-export const sequelize = new Sequilize(db.database, db.user, db.password, {
-    host: db.host,
-    dialect: db.dialect,
+export const sequelize = new Sequilize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
 });
